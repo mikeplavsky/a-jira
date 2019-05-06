9c157010defdf32f8770a8d013d1bce238287444
mikeplavsky <mike.plavsky@quest.com>
2019-05-06 11:48:56 +0300

    menu looks good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
