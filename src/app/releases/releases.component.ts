b15e02e556e32ebf9d83be64323cbdd032c91c96
mikeplavsky <mike.plavsky@quest.com>
2019-05-12 23:30:45 +0300

    added proxy for ng serve

--- src/app/releases/releases.component.ts
+++ src/app/releases/releases.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-releases',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReleasesComponent implements OnInit {
 
-  constructor() { }
+  product:any;
+
+  constructor(
+    private productsSvc: ProductsService,
+    private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.product = this.productsSvc.getProducts().filter(
+      x => x.id == id
+    );
   }
 
 }
