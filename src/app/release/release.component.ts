6f88fd781fef88f211b7bd78338d1e9a56250373
mikplavsky <mikhail.plavskiy@quest.com>
2019-06-02 01:51:28 +0300

    got points on screen

--- src/app/release/release.component.ts
+++ src/app/release/release.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import {Store, select, createSelector} from '@ngrx/store'
 import { FetchReleaseStats } from '../product-reducer'
+import { release } from 'os';
 
 @Component({
   selector: 'app-release',
@@ -12,17 +13,30 @@ export class ReleaseComponent implements OnInit {
 
   @Input() release:any;
   @Input() product:any;
+  releaseStats$: any;
 
   constructor(private store: Store<{}>){};
 
   ngOnInit() {
-    return
 
     this.store.dispatch(
       new FetchReleaseStats(
         this.product,
         this.release.key));
+    
+    let getReleaseStats = createSelector(
+      (state: {releases},props:{product,release}) => {
+        let p = state.releases[props.product];
+        return p ? p[props.release] : null;
+      });
+
+    this.releaseStats$ = this.store.pipe(
+      select(
+        getReleaseStats,
+        {
+          product: this.product, 
+          release: this.release.key
+        }));
 
   }
-
 }
