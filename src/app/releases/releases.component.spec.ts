6be5079ed7bde32766c3ecc66e3ca4ff8bbaf34d
mikplavsky <mikhail.plavskiy@quest.com>
2019-06-09 19:29:58 +0300

    started mocking store - it does not help yet

--- src/app/releases/releases.component.spec.ts
+++ src/app/releases/releases.component.spec.ts
@@ -3,19 +3,32 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReleasesComponent } from './releases.component';
 import { ReleaseComponent } from '../release/release.component';
 import { MaterialModule } from '../material.module';
-import { StoreModule } from '@ngrx/store';
+import { Store } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ReleasesComponent', () => {
+
   let component: ReleasesComponent;
   let fixture: ComponentFixture<ReleasesComponent>;
+  let store: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReleasesComponent, ReleaseComponent ],
-      imports: [ MaterialModule, StoreModule.forRoot({}), RouterTestingModule ]
+      declarations: [
+        ReleasesComponent, 
+        provideMockStore({ initialState: {
+          releases: {}
+        }}), 
+        ReleaseComponent ],
+      imports: [ 
+        MaterialModule,
+        RouterTestingModule ]
     })
     .compileComponents();
+    
+    store = TestBed.get(Store);
+
   }));
 
   beforeEach(() => {
