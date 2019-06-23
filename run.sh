aa8530202fe5622a61916d4c974b41475c0e5420
mikplavsky <mikhail.plavskiy@quest.com>
2019-06-24 00:44:10 +0300

    running docker as daemon

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-docker run -ti --rm \
+docker rm a-jira
+docker run -ti -d \
+    --name a-jira \
     -v $(pwd):/a-jira \
     -v /a-jira/node_modules \
     -p 4200:4200 \
-    -w /a-jira a-jira bash 
+    -w /a-jira a-jira \
+    bash ./server.sh
