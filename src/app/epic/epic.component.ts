8bd07ba19735d110cfa0e95131cbc3ed01323427
mikplavsky <mikhail.plavskiy@quest.com>
2019-07-15 01:09:58 +0300

    added statistics to epics

--- src/app/epic/epic.component.ts
+++ src/app/epic/epic.component.ts
@@ -14,14 +14,37 @@ export class EpicComponent implements OnInit {
   @Input() product;
   @Input() epic;
 
+  epicStats$;
+
   constructor(private store: Store<{}>){};
 
   ngOnInit() {
+
     this.store.dispatch(
       new FetchEpicStats(
         this.product,
         this.release,
         this.epic))
+
+    let getEpicStats = createSelector(
+
+      (state:{epicStats},props:{product,release,epic}) => {
+        
+        let p = state.epicStats[props.product];
+        if (!p) {return null;}
+
+        let r = p[props.release];
+        if (!r) {return null;}
+
+        return r[props.epic];});
+
+    let product = this.product;
+    let release = this.release;
+    let epic = this.epic;
+
+    this.epicStats$ = this.store.select(
+      getEpicStats, {product,release,epic});
+
   }
 
 }
