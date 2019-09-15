92c57b3e79aa7cc8ea22e4c0bcfe9b56c834d6fa
mikplavsky <mikhail.plavskiy@quest.com>
2019-09-15 13:18:18 +0300

    added gauge specs

---
+++ specs/products.spec
@@ -0,0 +1,15 @@
+# Products Page
+
+## Check Products 
+
+* Products are "RMADFE, ODM, RMAZ" 
+* Goto products page 
+* Page contains "ODM"
+* Page contains "RMAZ"
+* Page contains "RMADFE"
+
+## Check Velocity
+
+* "RMADFE" has the velocity "27" ps/sprint
+* Goto products page 
+* "RMADFE" got the velocity "27" ps/sprint
---
+++ tests/step_implementation.js
@@ -0,0 +1,53 @@
+/* globals gauge*/
+"use strict";
+const { 
+    intercept, openBrowser, write, 
+    closeBrowser, goto, press, text, 
+    below, focus, inputField, toRightOf } = require('taiko');
+
+const assert = require("assert");
+const headless = process.env.headless_chrome.toLowerCase() === 'true';
+
+beforeSuite(async () => {
+    await openBrowser({ headless: headless })
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+step("Products are <products>", async (products) => {
+
+    let body = products.split(",").map(p => {
+        return {name:p}; });
+
+    await intercept(
+        "/api/products", 
+        {body: JSON.stringify(body)});
+
+});
+
+step("Goto products page", async () => {
+    await goto('localhost:4200');
+});
+
+step("Page contains <content>", async (content) => {
+    assert.ok(await text(content).exists());
+});
+
+step("<product> has the velocity <velocity> ps/sprint", 
+    async (product, velocity) => {
+
+    await intercept(
+        `/api/products/${product}/features/done`, 
+        {body: `{"velocity":${velocity}}`});
+
+});
+
+step("<product> got the velocity <velocity> ps/sprint", 
+    async function(product, velocity) {
+
+    assert.ok(
+        await text(`${velocity}`, below(`${product}`)).exists());
+
+});
