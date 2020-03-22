8190187b5c89688bc44200a680d3527836f66a60
mikplavsky <mikhail.plavskiy@quest.com>
2020-03-22 21:12:05 +0300

    running chromium

---
+++ chromium.sh
@@ -0,0 +1,6 @@
+# https://github.com/getgauge/taiko/blob/master/lib/taiko.js#L258
+
+./chrome-mac/Chromium.app/Contents/MacOS/Chromium \
+    --remote-debugging-port=8001 \
+    --use-mock-keychain \
+    --enable-automation
